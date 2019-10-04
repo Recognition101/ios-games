d42226c36a25612aa321c3d65b96ff1fcbdc0bf4
Recognition101 <recognition101@gmail.com>
2019-10-03 22:53:47 -0700

    fix(tools) remove yargs
    chore(deps) update csv-parse to latest

--- tools/downloadImages.js
+++ tools/downloadImages.js
@@ -5,13 +5,32 @@ const fs    = require('fs');
 const https = require('https');
 const util  = require('util');
 const csv   = require('csv-parse/lib/sync');
-const argv  = require('yargs')
-    .option('all', {
-        alias: 'a',
-        describe: 'If given, re-download already existing images.'
-    })
-    .help('h')
-    .argv;
+
+/**
+ * Parses the CLI arguments and returns a map of them.
+ * @return {{[key: string]: boolean|string}} a map of argument keys to values
+ */
+const parseArgs = () => {
+    const result = /** @type {{[key: string]: boolean|string}} */({});
+    let lastKey = '';
+    for(const arg of process.argv.slice(2)) {
+        const isKey = arg.startsWith('-');
+        lastKey = isKey ? arg.replace(/^-*/, '') : lastKey;
+        result[lastKey] = isKey ? true : arg;
+    }
+    return result;
+};
+
+const helpMessage = `
+This command downloads the icon file for every game it can read in the local
+CSV file. This script was used before the migration from CSV to JSON.
+
+USAGE: downloadImages.js [OPTIONS]
+
+OPTIONS:
+    -h, --help  show this message and exit
+    -a, --all   If given, re-download already existing images
+`;
 
 const iTunesApi = 'https://itunes.apple.com/lookup?id=';
 const readFile  = util.promisify(fs.readFile);
@@ -35,15 +54,25 @@ const downloadFile = (url, path) => new Promise((yes, no) => {
 });
 
 const main = async () => {
+    // Parse Arguments, Display Help If Needed
+    const args = parseArgs();
+    const all  = args.a || args.all;
+    const help = args.h || args.help;
+
+    if (help) {
+        console.log(helpMessage);
+        return;
+    }
+
     const data = await readFile('../data.csv');
-    const rows = /** @type {Array<Array<string>>} */(csv(data));
+    const rows = /** @type {string[][]} */(csv(data));
 
     rows.shift();
 
     for(const row of rows) {
         const id      = (row[row.length - 1] || '').replace(/^id/, '');
         const imageFn = '../images/id' + id + '.jpg';
-        const skip    = !argv.all && fs.existsSync(imageFn);
+        const skip    = !all && fs.existsSync(imageFn);
 
         if (id && !skip) {
             const json = await fetchJson(iTunesApi + id);
